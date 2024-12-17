a081145c5a71aacb55476607cf68a4023649f61c
Topti Kafa <toprak311@hotmail.com>
2024-12-17 11:41:42 +0300

    Bigger Projekt Start
    FSM = Fuckin Sucker Machine

--- bigproject/character/character_scripts/input_manager.gd
+++
@@ -1,9 +0,0 @@
-class_name InputManager
-
-var x = 0
-var raw = Vector2.ZERO
-
-func update_input():
-	# Yatay eksen için giriş al
-	x = Input.get_axis("walk_left", "walk_right")
-	raw = Vector2(x, 0)  # Yatay eksende hareket
